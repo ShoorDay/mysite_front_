6e983a5415ffd84d3ec50b78a14074b101d1e22c
Sure <sure.z@outlook.com>
2020-02-15 11:03:55 +0800

    - 工信部链接

--- src/components/basic/Footer.vue
+++ src/components/basic/Footer.vue
@@ -5,7 +5,9 @@
       Shoor's Site ©2019 Created by Shoor
     </div>
     <div style="margin: 5px 0px;">
-      陕ICP备 19025064 号
+      <a href="http://beian.miit.gov.cn" style="color: inherit"
+        >陕ICP备 19025064 号</a
+      >
     </div>
     <div style="margin: 5px 0px;">
       若本站收录内容无意侵犯了贵公司版权，请给网页底部邮箱地址来信，我会及时处理和回复，谢谢合作
